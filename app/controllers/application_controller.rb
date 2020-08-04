92968c3f4b541cbf36a11022141844e34881ba25
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-08-04 16:44:45 +0900

    [fix] model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def not_authenticated
     flash[:warning] = t('defaults.message.require_login')
-    redirect_to login_path
+    redirect_to root_path
   end
 end
