c0b5004b9a29337936b37a66c1cddd665565301a
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-08-07 09:46:28 +0900

    [add] comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index create new]
+  resources :boards, only: %i[index new create show] do
+    resources :comments, only: %i[create], shallow: true
+  end
 end
