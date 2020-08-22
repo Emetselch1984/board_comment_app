328023fc1846dbed2cc4983ae48980f56769d72c
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-08-23 05:45:56 +0900

    test

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,13 @@
 class BookmarksController < ApplicationController
   def create
-    board = Board.find(prams[:board_id])
+    board = Board.find(params[:board_id])
     current_user.bookmark(board)
     redirect_back fallback_location: root_path, success: t('.success')
   end
+
   def destroy
-    board = Board.find(prams[:board_id])
+    board = current_user.bookmarks.find(params[:id]).board
     current_user.unbookmark(board)
-    redirect_back fallback_location: root_path, danger: t('.danger')
+    redirect_back fallback_location: root_path, success: t('.success')
   end
 end
