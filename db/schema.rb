47b04d607d0bca0727dd0fb82204c10025b3327e
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-09-05 09:33:48 +0900

    [add] profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_215656) do
+ActiveRecord::Schema.define(version: 2020_09_04_235259) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_215656) do
     t.datetime "updated_at", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
