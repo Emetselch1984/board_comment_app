0c078e8a3b23782bc54a883bf5aaeb5e62a57629
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-09-16 12:23:29 +0900

    [add] reset_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_031622) do
+ActiveRecord::Schema.define(version: 2020_09_16_032242) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -60,7 +60,12 @@ ActiveRecord::Schema.define(version: 2020_09_12_031622) do
     t.integer "role", default: 0
     t.string "first_name"
     t.string "last_name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
